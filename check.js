d51cd2b3cb284f2940219431f73dd3c2dc18786a
Alex Jongman <alex.jongman@hu.nl>
2020-03-29 22:10:44 +0200

    first commit

---
+++ check.js
@@ -0,0 +1,12 @@
+function check() {
+  const MAXNUMBER = 5;
+  for (let i = 0; i < MAXNUMBER; i += 1) {
+    let Value = 5;
+    console.log(Value);
+    Value += i;
+  }
+
+//   console.log(`done: ${Value}`);
+}
+
+check();
